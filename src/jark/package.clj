6bc827fca7b46b52d4d5711162f47543929df930
Martin DeMello <martindemello@gmail.com>
2012-01-22 05:12:06 -0800

    refactor project.clj

--- src/jark/package.clj
+++ src/jark/package.clj
@@ -5,101 +5,119 @@
   (:use [clojure.java.io :only (file copy)])
   (:refer-clojure :exclude [list find alias]))
 
+(defn hash-of [f1 f2 x]
+  (into {} (map #(vector (f1 %) (f2 %)) x)))
+
+(defn hash-of-assoc [x]
+  (hash-of first second x))
+
+(defn get-clojars-list [] (http-get-text-seq *clojars-all-jars-url*))
+
+(defn read-strings [response] (for [line response] (read-string line)))
+
+(defn project-path []
+  (str (get-cljr-home) (sep) project-clj))
+
 (defn list
   "List all packages that are installed"
   []
   (let [dependencies (:dependencies (get-project))]
-    (into {} (map #(vector (first %) (second %)) dependencies))))
+    (hash-of-assoc dependencies)))
+
+(defn matches-library [library-name]
+  #(= (first %) (symbol library-name)))
+
+(defn not-matches-library [library-name]
+  #(= (first %) (symbol library-name)))
+
+(defn update-deps-project [f]
+  (let [project (get-project)
+        dependencies (:dependencies project)
+        new-deps (f dependencies)
+        updated-project (assoc project :dependencies new-deps)
+        proj-str (project-clj-string updated-project {:dependencies new-deps})]
+    (spit (project-path) proj-str)))
 
 (defn install
   "Installs a given package name from available.the list of repositories
-   If the version is not provided, the latest version is assumed"
+  If the version is not provided, the latest version is assumed"
   ([library-name]
-     (let [version (get-latest-version library-name)]
-       (if version
-	 (install library-name version)
-	 (println "Cannot find version of" library-name "on Clojars.org.\r\n"
-		  "If the library is in another repository, provide a version argument."))))
+   (let [version (get-latest-version library-name)]
+     (if version
+       (install library-name version)
+       (println "Cannot find version of" library-name "on Clojars.org.\n"
+                "If the library is in another repository, provide a version argument."))))
+
   ([library-name library-version]
-    (let [project (get-project)
-	  dependencies (:dependencies project)
-	  updated-project (assoc project :dependencies
-				 (conj dependencies
-				       [(symbol library-name)
-					library-version]))
-	  proj-str (project-clj-string updated-project
-				       {:dependencies (:dependencies updated-project)})]
-      (println "Installing version " library-version " of " library-name "...")
-      (spit (str (get-cljr-home) (sep) project-clj) proj-str)
-      (deps (get-project)))
-    (str "Installed library " library-name , " - ", library-version)))
+   (println "Installing version " library-version " of " library-name "...")
+   (println "updating " (project-path))
+   (update-deps-project
+     (fn [dependencies]
+       (distinct (conj dependencies [(symbol library-name) library-version]))))
+   (deps (get-project))
+   (str "Installed library " library-name , " - ", library-version)))
+
+(defn uninstall [library-name]
+  (update-deps-project
+    (fn [dependencies]
+      (into [] (filter (not-matches-library library-name)
+                       dependencies))))
+  (str library-name " has been uninstalled"))
+
+(defn get-dep [d]
+  (apply merge (map #(hash-map (:tag %) (first (:content %))) d)))
 
 (defn get-library [d]
-  (let [dep (apply merge (map #(hash-map (:tag %) (first (:content %))) d))]
+  (let [dep (get-dep d)]
     (str (:groupId dep) "/" (:artifactId dep))))
 
 (defn get-version [d]
-  (let [dep (apply merge (map #(hash-map (:tag %) (first (:content %))) d))]
+  (let [dep (get-dep d)]
     (:version dep)))
 
 (defn dependencies
-  "Lists the dependecies for the given package name and version"
+  "Lists the dependencies for the given package name and version"
   [library-name version]
   (let [d (get-library-dependencies library-name version)]
-    (into {} (map #(vector (get-library %) (get-version %)) d))))
+    (hash-of get-library get-version d)))
 
 (defn versions
   "Lists the available versions for the package on remote repositories"
   [library-name]
-  (let [response (http-get-text-seq *clojars-all-jars-url*)
-        entries  (filter #(= (first %) (symbol library-name))
-                        (for [line response]
-                          (read-string line)))]
-    (into {} (map #(vector (first %) (second %)) entries))))
+  (let [response (get-clojars-list)
+        entries  (filter (matches-library library-name)
+                         (read-strings response))]
+    (hash-of-assoc entries)))
 
 (defn search
   [term]
-  (let [response (http-get-text-seq *clojars-all-jars-url*)
+  (let [response (get-clojars-list)
         entries  (for [line response :when (.contains line term)]
                    (read-string line))]
-    (into {} (map #(vector (first %) (second %)) entries))))
+    (hash-of-assoc entries)))
 
 (defn latest-version [library-name]
-  (let [response (http-get-text-seq *clojars-all-jars-url*)
-	lib-name (symbol library-name)]
-    (second (last (filter #(= (first %) lib-name)
-			  (for [line response]
-			    (read-string line)))))))
-
-(defn uninstall [library-name]
-  (let [project (get-project)
-	dependencies (:dependencies project)
-	updated-project (assoc project :dependencies
-			       (into [] (filter #(not= (symbol library-name)
-						       (first %))
-						dependencies)))
-	proj-str (project-clj-string updated-project
-				     {:dependencies (:dependencies updated-project)})]
-    (spit (str (get-cljr-home) (sep) project-clj) proj-str)
-    (str library-name " has been uninstalled")))
+  (let [response (get-clojars-list)]
+    (second (last (filter (matches-library library-name)
+                          (read-strings response))))))
 
 (defn repo-list []
   (let [repos (merge leiningen.pom/default-repos
-		     (:repositories (get-project)))]
-    (into {} (map #(vector (first %) (second %)) repos))))
+                     (:repositories (get-project)))]
+    (hash-of-assoc repos)))
+
+(defn write-repo-file [repo-map]
+  (spit (project-path)
+        (project-clj-string (get-project) {:repositories repo-map})))
 
 (defn repo-add
   ([repo-name repo-url]
-     (let [repo-map (assoc (get-repositories) repo-name repo-url)]
-       (spit (file (str (get-cljr-home) (sep) project-clj))
-	     (project-clj-string (get-project)
-				 {:repositories repo-map}))
-       (str repo-name " repository added"))))
+   (let [repo-map (assoc (get-repositories) repo-name repo-url)]
+     (write-repo-file repo-map)
+     (str repo-name " repository added"))))
 
 (defn repo-remove
   ([repo-name]
-     (let [repo-map (dissoc (get-repositories) repo-name)]
-       (spit (file (str (get-cljr-home) (sep) project-clj))
-	     (project-clj-string (get-project)
-				 {:repositories repo-map}))
-       (str repo-name " repository removed"))))
+   (let [repo-map (dissoc (get-repositories) repo-name)]
+     (write-repo-file repo-map)
+     (str repo-name " repository removed"))))
