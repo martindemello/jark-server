3c4e698527cf623e9253bd603bb4bf11436669df
Martin DeMello <martindemello@gmail.com>
2012-01-22 18:02:15 -0800

    name to 0.4-SNAPSHOT so we can depend on other snapshots

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jark "0.4"
+(defproject jark "0.4-SNAPSHOT"
   :description "JARK is a tool to manage classpaths and clojure namespaces on a persistent JVM"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
@@ -9,4 +9,4 @@
 
   :java-source-path "src/jark"
   :aot [ leiningen.pom jark.vm jark.cp jark.ns jark.swank jark.pp jark.doc jark.package
-        cljr.core cljr.clojars jark.lein]) 
+        cljr.core cljr.clojars jark.lein])
